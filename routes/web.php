a602fa5d102258e2cabf89320f100ab448f75a39
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-12-29 23:33:06 +0200

    Solve doublication

--- routes/web.php
+++ routes/web.php
@@ -7,47 +7,35 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '%50,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Developer',
+        'salary' => '%30,000'],
+    [
+        'id' => 3,
+        'title' => 'Designer',
+        'salary' => '%20,000'
+    ]];
+
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
         "jobs" => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '%50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '%30,000'],
-            [
-                'id' => 3,
-                'title' => 'Designer',
-                'salary' => '%20,000'
-            ],
+            $jobs
         ]]);
-
 });
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '%50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Developer',
-            'salary' => '%30,000'],
-        [
-            'id' => 3,
-            'title' => 'Designer',
-            'salary' => '%20,000'
-        ],
+        $jobs
     ];
 
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
 //    dd($job);
 
     return view('job', ['job' => $job]);

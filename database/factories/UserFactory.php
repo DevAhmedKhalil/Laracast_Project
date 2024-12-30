d235bc93182571228686d4fdadbe8381f22efbdd
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-12-30 22:54:11 +0200

    Model factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use function Webmozart\Assert\Tests\StaticAnalysis\false;
+use function Webmozart\Assert\Tests\StaticAnalysis\true;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,11 +26,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first-name' => fake()->firstName(),
+            'last-name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+//            'admin' => false
         ];
     }
 
@@ -37,8 +41,17 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
+
+
+//    public function admin(): static
+//    {
+//        return $this->state(fn(array $attributes) => [
+//            'admin' => true,
+//        ]);
+//        // User::factory()->admin()->create();
+//    }
 }

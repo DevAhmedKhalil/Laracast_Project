fb7564bfcccecbe0d2e7e0666577096f2e6884a8
DevAhmedKhalil <dev.akhalil01@gmail.com>
2025-01-02 21:11:20 +0200

    Starter Kits, Breeze, and Middleware

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
-
-//Route::controller(JobController::class)->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/create', 'create');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
-
-//Route::resource('jobs', JobController::class, [
-////    'only' => ['index', 'show'],
-////    'except' => ['edit']
-//]);
-
 Route::resource('jobs', JobController::class);
 
